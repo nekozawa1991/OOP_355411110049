3a876f4f67c00bdd4550f592366b5c663d967825
nekozawa1991 <mingzhu.mizu@gmail.com>
2017-11-19 14:48:53 +0700

    Testing upload to github

---
+++ src/oop_lab/s355411110049/rmutsv/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package oop_lab.s355411110049.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main_method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I am Frame");
+
+}//main
+
+}//class
